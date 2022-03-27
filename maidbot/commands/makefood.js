7162f531ca9a2e6368617249d71dc2bda19c6286
Jimmytrump56 <82419990+Jimmytrump56@users.noreply.github.com>
2022-03-27 14:51:27 -0400

    ma

--- maidbot/commands/makefood.js
+++ maidbot/commands/makefood.js
@@ -14,7 +14,7 @@ module.exports = {
 
           iao = new MessageEmbed()
           .setTitle("Made food!")
-          .setImage(imgss)
+          .setImage("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.printmag.com%2Fdesign-news%2Fsomeone-just-bought-a-gif-for-half-a-million-dollars%2F&psig=AOvVaw3D0NSC1yHWUwHTITSG6ZRT&ust=1648493448004000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJis-OT65vYCFQAAAAAdAAAAABAD")
           .setDescription(`${message.author} I have finished making your food!`)
           
 
